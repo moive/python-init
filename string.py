300af930a64046bbdd83f6d4e193968cac8bc17c
Moises Velasquez <mvelasquez@pictoric.com>
2020-11-18 18:19:43 -0500

    variables concatenation

--- string.py
+++ string.py
@@ -1,5 +1,11 @@
-print("Asigncion: ")
+print("Asignacion: ")
 mensaje = "Hola"
 mensaje += " "
 mensaje += "Moises"
 print(mensaje)
+
+print("Concatenacion")
+message = "Hello"
+space = " "
+name = "Daniela"
+print(message + space + name)
