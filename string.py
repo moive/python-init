48c80fada3fef22476a56cacb20fefe92da93b38
Moises Velasquez <mvelasquez@pictoric.com>
2020-11-18 18:47:19 -0500

    search string

--- string.py
+++ string.py
@@ -16,3 +16,10 @@ number_two = 32
 result = number_one + number_two
 result = str(result)
 print("The result of this sum is: " + result)
+
+print("Search")
+
+text = "Welcome to my website"
+substring = text.find("website")
+print("website is position: ")
+print(substring)
